c9b17d9556ac2bc5fdd11814763b2a9ad0078e75
slime7 <i@7sli.me>
2022-12-19 09:02:50 +0800

    Initial commit

---
+++ src/ui.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container fluid class="plugin-main">
+    <div>hello translime</div>
+
+    <div>
+      <v-btn>vuetify button</v-btn>
+    </div>
+  </v-container>
+</template>
+
+<script>
+const vuetify = window.vuetify$.components;
+
+export default {
+  name: 'ui',
+
+  components: {
+    ...vuetify,
+  },
+};
+</script>
---
+++ ui.vite.config.js
@@ -0,0 +1,46 @@
+import { builtinModules } from 'module';
+import { createVuePlugin } from 'vite-plugin-vue2';
+import pkg from './package.json';
+
+/**
+ * @type {import('vite').UserConfig}
+ * @see https://vitejs.dev/config/
+ */
+const config = {
+  plugins: [
+    createVuePlugin(),
+  ],
+  envDir: process.cwd(),
+  build: {
+    sourcemap: false,
+    target: 'node14',
+    outDir: './dist',
+    terserOptions: {
+      ecma: 2021,
+      compress: {
+        passes: 2,
+      },
+      safari10: false,
+    },
+    lib: {
+      entry: 'src/ui.vue',
+      name: pkg.name, // 需要指定一个唯一 id
+      fileName: (format) => `ui.${format}.js`,
+    },
+    cssCodeSplit: true,
+    rollupOptions: {
+      external: [
+        'vue',
+        ...builtinModules,
+      ],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+    emptyOutDir: false,
+  },
+};
+
+export default config;
